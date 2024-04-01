e38d97aba09867002ab1d770c8f63f78ae09f995
Mohammad Jayeed <thisisjayeed@gmail.com>
2024-04-01 17:20:43 +0600

    added ERD

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Blog(models.Model):
-    
-    
+   
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=2000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
